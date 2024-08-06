ab7ddf99495d26ce58b17efd9324fe753777371e
Paddy McBreen <paddymcbreen@CB-L132.local>
2024-08-06 16:21:21 +0100

    vpc resource + subnet resource created for blue green deployment - shells

--- MySQL/main.tf
+++ MySQL/main.tf
@@ -1,5 +1,14 @@
-resource "aws_vpc" "main" {
+resource "aws_vpc" "mysql_vpc" {
   cidr_block = "10.0.0.0/16"
+  
+}
+
+resource "aws_subnet" "public_mysql_subnet" {
+  vpc_id     = aws_vpc.mysql_vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  count = 1
+
 }
 
 resource "aws_db_instance" "mysql_db_instance" {
