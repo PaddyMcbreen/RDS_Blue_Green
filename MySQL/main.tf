5b946c94a764ed849e93f5fcd1dcd7047db8a9f4
Paddy McBreen <paddymcbreen@CB-L132.local>
2024-08-06 16:27:22 +0100

    igw created, public route table created - route table associations created - shells

--- MySQL/main.tf
+++ MySQL/main.tf
@@ -30,3 +30,28 @@ resource "aws_db_instance" "mysql_db_instance" {
   }
 
 }
+
+# Internet gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main_vpc.id
+
+}
+
+# Public route table
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.mysql_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+}
+
+# Route table associations for public subnets
+resource "aws_route_table_association" "public_route" {
+  count = 1
+
+  subnet_id      = aws_subnet.public_mysql_subnet.id
+  route_table_id = aws_route_table.public_route_table.id
+}
